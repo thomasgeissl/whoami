ea6495cc24ecabbfdc37debde10aefd53254f006
Thomas Geissl <thomas.geissl@gmail.com>
2020-12-14 12:07:31 +0100

    added some more content, still wip

--- src/md/software.md
+++ src/md/software.md
@@ -5,6 +5,10 @@ id: "software"
 Some of my open source contributions can be found on my [github page](https://github.com/thomasgeissl).
 
 - [ofPackageManager](https://github.com/thomasgeissl/ofPackageManager): package manager for openFrameworks
+- [ofPackageManager-frontend](https://github.com/thomasgeissl/ofPackageManager-frontend): its electron, react based frontend
 - [ofOSCDebugger](https://github.com/thomasgeissl/ofOSCDebugger), [ofMIDIDebugger](https://github.com/thomasgeissl/ofMIDIDebugger): osc and midi debugging tools
+- [ofMIDI2MQTT](https://github.com/thomasgeissl/ofMIDI2MQTT), [ofOSC2MIDI](https://github.com/thomasgeissl/ofOSC2MIDI), ...: small cli utilities to convert between different protocols
 - [ofxLiveSet](https://github.com/thomasgeissl/ofxLiveSet), [ofxNDI](https://github.com/thomasgeissl/ofxNDI), ...: openFrameworks addons
 - [QMIDI](https://github.com/thomasgeissl/QMIDI), [QOSC](https://github.com/thomasgeissl/QOSC), [QFirmata](https://github.com/thomasgeissl/QFirmata), ...: Qt libraries
+
+- [gitable](https://github.com/thomasgeissl/gitable): small cli utility to enable git usage for ableton live projects
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,69 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { Helmet } from "react-helmet"
-import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
 
-import grantler0 from "../assets/images/grantlersoundsystem/06072019.png"
-import grantler1 from "../assets/images/grantlersoundsystem/14082019.png"
-import klanglichtstrom from "../assets/videos/klanglichtstrom.mp4"
 import laparesse from "../assets/audio/la_paresse.mp3"
+import ohnmacht from "../assets/audio/ohnmacht.mp3"
 import blt from "../assets/images/nantlab/blt.jpg"
-import vj from "../assets/images/nantlab/vj.jpg"
+import vgig from "../assets/images/vgig/screenshot.png"
+import styled from "styled-components"
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-}))
+import "../styles/global.scss"
 
+const Container = styled.div`
+  margin: auto;
+  max-width: 1024px;
+  background-color: rgb(255, 255, 255);
+  color: rgb(0, 0, 0);
+  font-size: 24px;
+  audio,
+  img {
+    width: 100%;
+    margin-top: 64px;
+    margin-bottom: 64px;
+  }
+`
+const List = styled.ul`
+  list-style-type: square;
+`
+const Project = styled.li`
+  margin-top: 72px;
+  margin-bottom: 72px;
+  &:nth-child(2) {
+  }
+  .rightAlign {
+    padding-left: 24px;
+    text-align: right;
+  }
+`
+
+const Date = styled.div`
+  font-style: italic;
+  text-align: right;
+  margin-top: 24px;
+  margin-bottom: 24px;
+  &::before {
+    content: "[";
+  }
+  &::after {
+    content: "]";
+  }
+`
+
+const CV = styled.div``
+const Footer = styled.footer`
+  margin-top: 128px;
+`
 export default function Template({ data }) {
-  const classes = useStyles()
-
-  const { about, projects, software, footer } = data
+  const { about, software, footer } = data
   return (
-    <div>
+    <Container>
       <Helmet>
         <meta charSet="utf-8" />
         <title>whoami</title>
       </Helmet>
-      <div className={classes.root}>
+      <div>
         <div
           className="about"
           dangerouslySetInnerHTML={{ __html: about.html }}
@@ -36,83 +72,174 @@ export default function Template({ data }) {
           className="projects"
           // dangerouslySetInnerHTML={{ __html: projects.html }}
         >
-          Here is a small selection of projects I have worked on.
-          <ul>
-            <li>
-              Spinning vinyls with the grantler soundsystem dj collective.
-              Mostly playing postpunk, 80s, dark wave, shoegaze and dreampop.{" "}
-              <br></br>
-              <Grid container spacing={2} className="media">
-                <Grid item xs={12} sm={3}>
-                  <img src={grantler0}></img>
-                </Grid>
-                <Grid item xs={12} sm={3}>
-                  <img src={grantler1}></img>
-                </Grid>
-              </Grid>
-              A spotify playlist is available{" "}
-              <a href="https://open.spotify.com/playlist/5bQtfOpbN2hoAmS1groTED?si=9w7twhWCTKeu1ccWQLNutg">
-                here
-              </a>
-              .<div className="date">since 2019</div>
-            </li>
-            <li>
-              <a href="https://www.instagram.com/explore/tags/klanglichtstrom/">
-                klanglichtstrom
-              </a>{" "}
-              is an audio visual performance by{" "}
-              <a href="http://www.benbengler.com">Ben Bengler</a> and me. We
-              developed a custom software based on{" "}
-              <a href="https://github.com/thomasgeissl/ofxLiveSet">
-                ofxLiveSet
-              </a>{" "}
-              to extract audio features and create light patterns.
+          <p>
+            Unfortunately, I cannot show most of the projects I have worked on
+            due to confidentiality reasons. But here is a small selection of
+            projects I have worked on aside from my job.
+          </p>
+
+          <p>
+            Please note that this is a work in progress and very likely
+            outdated.
+          </p>
+          <List>
+            <Project>
+              <p>
+                <a href="https://thomasgeissl.github.io/vjam/" target="_blank">
+                  vjam
+                </a>{" "}
+                and{" "}
+                <a href="https://thomasgeissl.github.io/vgig/" target="_blank">
+                  vgig
+                </a>{" "}
+                are two collaborative musical experiments. They started as
+                weekend projects during the Covid-19 pandemic. vjam is a virtual
+                jam session system, where users can create virtual jam session
+                rooms and play music together. vgig is virtual concert hall
+                system. it adds omni directional communication to virtual
+                concerts, communication between audience and performers, as well
+                as audience and audience. vgig is a collaboration with artist
+                and composer{" "}
+                <a
+                  href="https://www.instagram.com/windysetiadi/"
+                  target="_blank"
+                >
+                  Windy Setiadi
+                </a>{" "}
+                and was shown at{" "}
+                <a href="https://mag20.mediaartglobale.com/#" target="_blank">
+                  Media Art Globale
+                </a>{" "}
+                2020.
+              </p>
+              <img src={vgig}></img>
+              <p>
+                Both projects are built with react, tonejs and mqtt. vgig
+                additionally uses react-three-fiber, a threejs react reconciler.
+                vjam and vgig run on github pages and are deployed via travis
+                continous integration.
+              </p>
+              <p>A proper describtion will follow.</p>
+              <Date>in 2020</Date>
+            </Project>
+            <Project>
+              <p>
+                I wrote a little multi channel video looper for Residenztheater
+                Muenchen. It is able to capture video from live video sources or
+                load video clips from the hard disk. The software is fully midi
+                controllable, it can be synchronised with external midi clocks.
+              </p>
+              <p>It is built with openFrameworks.</p>
+              <Date>in 2020</Date>
+            </Project>
+
+            <Project>
               <Grid container className="media">
                 <Grid item xs={12} sm={4}>
-                  <video src={klanglichtstrom} controls></video>
+                  <p>
+                    <a href="https://www.instagram.com/explore/tags/klanglichtstrom/">
+                      klanglichtstrom
+                    </a>{" "}
+                    is an audio visual performance by{" "}
+                    <a href="http://www.benbengler.com">Ben Bengler</a> and me.
+                    We developed a custom software based on{" "}
+                    <a href="https://github.com/thomasgeissl/ofxLiveSet">
+                      ofxLiveSet
+                    </a>
+                    . It extracts audio features and create light patterns.
+                  </p>
+                  <p>It is built with openFrameworks and JUCE.</p>
+                </Grid>
+                <Grid item xs={12} sm={8} className="rightAlign">
+                  <iframe
+                    src="https://player.vimeo.com/video/385237672"
+                    width="640"
+                    height="360"
+                    frameborder="0"
+                    allow="autoplay; fullscreen"
+                    allowfullscreen
+                  ></iframe>
                 </Grid>
               </Grid>
-              Video by{" "}
-              <a href="https://www.instagram.com/dava_dnb/">dava_dnb</a> taken
-              at <a href="https://reservoir.space">reservoir festival</a>.
-              <div className="date">since 2018</div>
-            </li>
-            <li>
-              Guitars and electronics at glumb band. We are currently working on
-              our first ep "honesticide" (working title). A very early demo
-              snippet of "la paresse" is available here:<br></br>
-              <audio src={laparesse} controls className="media"></audio>
-              <br></br>
-              It uses some custom plugins, e.g.{" "}
-              <a href="https://github.com/thomasgeissl/esrever">esrever</a>.
-            </li>
-            <li>
+
+              <Date>since 2018</Date>
+            </Project>
+
+            <Project>
+              <p>
+                <a href="https://grantlerrecords.com/" target="_blank">
+                  grantler records
+                </a>{" "}
+                is a music label founded in 2020. Following the diy spirit, we
+                wanna support honest music that touches us. Besides the label,
+                we run a small blog and dj collective.
+              </p>
+              <p>
+                The blogging software is written with gastbyjs, hosted on github
+                pages, deployed via travis continous integration.
+              </p>
+            </Project>
+
+            <Project>
+              As written earlier, music is a big thing in my life.
+              <ul>
+                <li>
+                  Guitars and electronics at glumb band. A very early demo
+                  snippet of "la paresse" is available here:<br></br>
+                  <audio src={laparesse} controls></audio>
+                </li>
+                <li>
+                  Keys and electronics at{" "}
+                  <a
+                    href="https://grantlerrecords.com/artists/silent-sex"
+                    target="_blank"
+                  >
+                    Silent Sex
+                  </a>
+                  .<audio src={ohnmacht} controls></audio>
+                </li>
+              </ul>
+              <p>
+                Some of our tracks use custom made plugins, e.g.{" "}
+                <a href="https://github.com/thomasgeissl/esrever">esrever</a>.
+              </p>
+              <Date>since 2018</Date>
+            </Project>
+            <Project>
               <a href="https://www.facebook.com/events/1734744333425505/">
                 sharepool
               </a>{" "}
               is a video opera in collabration with the breakout ensemble. Live
               video mixing and manipulation with a custom made software.
-              <div className="date">in 2016</div>
-            </li>
-            <li>
-              Several light concepts and development of{" "}
-              <a href="http://www.martinlerf.de/projects/polywand/">
-                polygon wall
-              </a>
-              , an interactive, kinetic installation in collaboration with{" "}
-              <a href="http://www.martinlerf.de/">Martin Lerf</a>.
-              <iframe
-                src="https://player.vimeo.com/video/135904342"
-                width="640"
-                height="360"
-                frameborder="0"
-                allow="autoplay; fullscreen"
-                allowfullscreen
-                className="media"
-              ></iframe>
-              <div className="date">from 2014 to 2015</div>
-            </li>
-            <li>
+              <Date>in 2016</Date>
+            </Project>
+            <Project>
+              <Grid container className="media">
+                <Grid item xs={12} sm={4}>
+                  Several light concepts and development of{" "}
+                  <a href="http://www.martinlerf.de/projects/polywand/">
+                    polygon wall
+                  </a>
+                  , an interactive, kinetic installation in collaboration with{" "}
+                  <a href="http://www.martinlerf.de/">Martin Lerf</a>.
+                </Grid>
+                <Grid item xs={12} sm={8} className="rightAlign">
+                  <iframe
+                    title="polywand"
+                    src="https://player.vimeo.com/video/135904342"
+                    width="640"
+                    height="360"
+                    frameborder="0"
+                    allow="autoplay; fullscreen"
+                    allowfullscreen
+                    className="media"
+                  ></iframe>
+                </Grid>
+              </Grid>
+
+              <Date>from 2014 to 2015</Date>
+            </Project>
+            <Project>
               Several interactive installations with{" "}
               <a href="https://www.instagram.com/nantlab/">nantlab</a>, ranging
               from interior design to live visuals and everything in between.
@@ -121,50 +248,46 @@ export default function Template({ data }) {
                 Bar Love Triangle
               </a>{" "}
               a modular, interactive furniture and interior design.
-              <Grid container spacing={2} className="media">
-                <Grid item xs={12} sm={3}>
-                  <img src={blt}></img>
+              <img src={blt} alt="bar love triangle"></img>
+              <Date>since 2012</Date>
+            </Project>
+            <Project>
+              <Grid container>
+                <Grid item xs={12} sm={4}>
+                  Several performances in collabration with Kapitel Zwei
+                  Kolektif, such as the{" "}
+                  <a href="http://www.twoinarow.com/2013/09/konterkunst-konkordia">
+                    Dauerperformance
+                  </a>{" "}
+                  at Schaustelle (
+                  <a href="https://www.pinakothek.de/besuch/sammlung-moderne-kunst-der-pinakothek-der-moderne">
+                    Pinakothek der Moderne
+                  </a>
+                  ).
                 </Grid>
-                <Grid item xs={12} sm={3}>
-                  <img src={vj}></img>
+                <Grid item xs={12} sm={8} className="rightAlign">
+                  <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/8ZhofVerLbA"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                  ></iframe>
                 </Grid>
               </Grid>
-              <div className="date">since 2012</div>
-            </li>
-            <li>
-              Several performances in collabration with Kapitel Zwei Kolektif,
-              such as the{" "}
-              <a href="http://www.twoinarow.com/2013/09/konterkunst-konkordia">
-                Dauerperformance
-              </a>{" "}
-              at Schaustelle (
-              <a href="https://www.pinakothek.de/besuch/sammlung-moderne-kunst-der-pinakothek-der-moderne">
-                Pinakothek der Moderne
-              </a>
-              ).
-              <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/8ZhofVerLbA"
-                frameborder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-                className="media"
-              ></iframe>
-              <div className="date">from 2012 to 2015</div>
-            </li>
-          </ul>
+
+              <Date>from 2012 to 2015</Date>
+            </Project>
+          </List>
         </div>
         <div
           className="software"
           dangerouslySetInnerHTML={{ __html: software.html }}
         />
-        <div
-          className="footer"
-          dangerouslySetInnerHTML={{ __html: footer.html }}
-        />
+        <Footer dangerouslySetInnerHTML={{ __html: footer.html }} />
       </div>
-    </div>
+    </Container>
   )
 }
 export const pageQuery = graphql`
